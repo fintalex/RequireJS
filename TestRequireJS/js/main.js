4a193c00b6283fc74e3d6923bee6ad1c3d7ecf8d
fintalex <fintalex@mail.ru>
2015-03-31 11:11:39 +0300

    First controller and view

--- TestRequireJS/js/main.js
+++ TestRequireJS/js/main.js
@@ -1,3 +1,15 @@
-require(['app/MyMath'], function(MyMath2) {
-	console.log(MyMath2.add(1,2));
-})
+require(['Models/User', 'Controllers/ListController'], function(User, ListController){
+
+	var users = [ 	new User('Vasya'),
+					new User('Misha'),
+					new User('Petia')];
+
+	for (var i = 0, len = users.length; i < len; i++) {
+		console.log(users[i].name);
+	}
+
+	localStorage.users = JSON.stringify(users);
+
+	ListController.start();
+
+});
